be2be22612af686df629e95dcfdb279ddac8fcf8
Rinshad <learn.rinshad@gmail.com>
2025-07-23 17:10:00 +0530

    modify number guessing game to add scoreboard and high score tracking

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -14,18 +14,18 @@ def generate_number(level_choice):
         target_number = random.randint(1, 100)
         return target_number, 1, 100
     elif level_choice == 4:
-        while True:
+        while True: #Keep asking until a valid custom range is entered
             min_limit = validate_number("Please enter your minimum limit: ")
             max_limit = validate_number("Please enter your maximum limit: ")
-
-            if min_limit >= max_limit:
+            if min_limit >= max_limit: #Check if minimum value is less than maximum
                 print(f"\nYou select {min_limit} and {max_limit}\nMinimum limit must be less than maximum limit.\n")
-                continue
+                continue 
             target_number = random.randint(min_limit, max_limit)
             return target_number, min_limit, max_limit
     else:
         print("invalid")
         
+#Validates that the user's input is a number
 def validate_number(msg):
     while True:
         try:
@@ -33,16 +33,38 @@ def validate_number(msg):
         except ValueError:
             print("Invalid number. Please provide a valid number.\n")
 
+#global variables
+total_score = []
+high_score = 0
 
+#To calculate and updates the score
+def calculate_score(result):
+    global high_score 
+    total_score.append(result)
+    if result > high_score:
+        high_score = result #Updates high score globally if current score is higher
+    print(f"\nYour Highest Score is {high_score}")
+    print("ScoreBoard: ") #Displays all previous scores
+    for i in total_score:
+        print(i, end=" ")
+        
+#Main Function
 def main():
+    print(type(total_score))
     while True:
         game_instruction = """
     Welcome to the Number Guessing Game!
 
+    I will pick a secret number, and your goal is to guess it before you run out of tries.
+
     How to play:
-    - I will choose a secret number.
-    - You need to guess it before you run out of attempts.
-    - After each guess, I will give you a hint: too high or too low..
+    - Choose your difficulty level (Easy, Medium, Hard, or Custom).
+    - In Custom mode, you pick your own number range and number of guesses.
+    - I will let you know if your guess is too high or too low.
+    - Each wrong guess lowers your remaining attempts and your current score.
+    - Your final score is the number of guesses you had left when you found the secret number!
+    - If you run out of attempts, you will see the correct answer.
+    - After each round, your score is shown on the scoreboard (including your all-time high score!).
 
     Choose your difficulty level:
         1. Easy   --> Guess a number from 1 to 10  (5 attempts)
@@ -73,7 +95,7 @@ def main():
                 break
             elif level_choice == 4:
                 target_number, min_limit, max_limit = generate_number(4)
-                attempt_left = validate_number("How many guesses would you like?")
+                attempt_left = validate_number("How many guesses woulhigh_score = 0d you like?")
                 print(f"\nYou picked Custom Level!\nGuess a number between {min_limit} and {max_limit}.\nYou have {attempt_left} tries. Good luck!\n")
                 break
             else:
@@ -81,28 +103,35 @@ def main():
                 continue
         
         count = 0
+        score = attempt_left
     
         while attempt_left > 0:
             user_guess = validate_number("Enter Your Guess : ")     
             if user_guess < min_limit or user_guess > max_limit:
-                print(f"\nThat guess is  out of range,\nPlease enter an number between {min_limit} and {max_limit}\n")
+                print(f"\nThat guess is  out of range,\nPlease enter a number between {min_limit} and {max_limit}\n")
             else:
                 count += 1
                 if user_guess == target_number:
                     print(f"Congratulations! You guessed the number in {count} tries!")
+                    
                     break
                 elif user_guess > target_number:
                     print("Too high! Try a lower number.")
                 else:
                     print("Too low! Try a higher number.")
-                attempt_left -= 1
+    
+                attempt_left -= 1 #Decrease the number of remaining attempts
+                score -= 1  #Decrese score
                 print(f"You have only {attempt_left} attempt left\n")
 
                 if attempt_left <= 0:
                     print("Game over! You have used all your attempts.")
                     print(f"The correct number was {target_number}.\nBetter luck next time!\n")
-        while True:
-            play_again = input("Do you want to play again (y/n)").lower()
+                    
+        calculate_score(score) #Calculate and display score after the game ends
+        
+        while True:  #Playagain
+            play_again = input("\n\nDo you want to play again (y/n): ").lower()
 
             if play_again == "y" or play_again == "n":
                 break
@@ -114,6 +143,7 @@ def main():
             continue
         else:
             print("\nThanks For Playing!!\n\nGood Bye\n")
+            print(f"You Final Scoreboard {total_score} and your highest score is {high_score}\n")
             break
                 
 if __name__ == "__main__":
