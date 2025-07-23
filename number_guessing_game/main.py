a4f9889716c089ec7ef421888a42f222fe97dd0c
Rinshad <learn.rinshad@gmail.com>
2025-07-23 14:49:56 +0530

    modify number guessing game to implement difficulty levels and enhance user experience

--- number_guessing_game/main.py
+++ number_guessing_game/main.py
@@ -1,93 +1,105 @@
-# Number Guessing Game Using Python
+#Number Guessing Game Using Python
 import random
 
-def generate_number():
-    while True:
-        number_choice = input(
-            "Would you like to set your own number range? (y/n) : ").strip().lower()
-        if number_choice == "y":
-            while True:
-                min_limit = validate_number("Please enter your minimum limit: ")
-                max_limit = validate_number("Please enter your maximum limit: ")
+def generate_number(level_choice):
+    if level_choice == 1:
+        target_number = random.randint(1, 10)
+        return target_number, 1, 10
+    
+    elif level_choice == 2:
+        target_number = random.randint(1, 50)
+        return target_number, 1, 50
+    
+    elif level_choice == 3:
+        target_number = random.randint(1, 100)
+        return target_number, 1, 100
+    elif level_choice == 4:
+        while True:
+            min_limit = validate_number("Please enter your minimum limit: ")
+            max_limit = validate_number("Please enter your maximum limit: ")
 
-                if min_limit >= max_limit:
-                    print(f"\nYou select {min_limit} and {max_limit}\nMinimum limit must be less than maximum limit.\n")
-                    continue
-                target_number = random.randint(min_limit, max_limit)
-                return target_number, min_limit, max_limit
-
-        elif number_choice == "n":
-            print("Using the default range (1-10)")
-            target_number = random.randint(1, 10)
-            return target_number, 1, 10
-
-        else:
-            print("Invalid Option! Please choose (y/n)\n")
-
-
+            if min_limit >= max_limit:
+                print(f"\nYou select {min_limit} and {max_limit}\nMinimum limit must be less than maximum limit.\n")
+                continue
+            target_number = random.randint(min_limit, max_limit)
+            return target_number, min_limit, max_limit
+    else:
+        print("invalid")
+        
 def validate_number(msg):
     while True:
         try:
             return int(input(msg))
         except ValueError:
-            print("Invalid number. Please provide a valid number.")
-
-
-def generate_limit():
-    while True:
-        limit_choice = input(
-            "\nWould you like to set your own guess limit? (y/n): ").strip().lower()
-        if limit_choice == "y":
-            # attempt = int(input("How many guesses would you like? : "))
-            attempt = validate_number("How many guesses would you like?")
-            return attempt
-
-        elif limit_choice == "n":
-            print("Using the default attempt limit (5)")
-            return 5
-        else:
-            print("Invalid Choice!!\nPlease Choose a correct option.")
+            print("Invalid number. Please provide a valid number.\n")
 
 
 def main():
-    instructions = """
+    game_instruction = """
 Welcome to the Number Guessing Game!
 
-*** HOW TO PLAY ***
+How to play:
+- I will choose a secret number.
+- You need to guess it before you run out of attempts.
+- After each guess, I will give you a hint: too high or too low..
 
-1. At the start, you can choose your own number range or use the default (1-10).
-2. Next, you can set how many guesses (attempts) you'd like, or use the default (5).
-3. Try to guess the secret number within your selected range.
-4. After each guess, you'll be told if your guess was too high or too low.
-5. If you guess the number within your allowed attempts, you win!
-6. If you run out of guesses, the game will reveal the secret number.
+Choose your difficulty level:
+    1. Easy   --> Guess a number from 1 to 10  (5 attempts)
+    2. Medium --> Guess a number from 1 to 50  (6 attempts)
+    3. Hard   --> Guess a number from 1 to 100 (7 attempts)
+    4. Custom --> Choose your own range and attempts
 
 Good luck and have fun!
 """
-
-    print(instructions)
-    target_number, min_limit, max_limit = generate_number()
-    attempt_left = generate_limit()
-    count = 0
-    print(
-        f"\nGuess a number between {min_limit} and {max_limit}.\nYou have {attempt_left} tries. Good luck!\n")
-    while attempt_left > 0:
-        user_guess = validate_number("Enter Your Guess : ")
-        count += 1
-        if user_guess == target_number:
-            print(f"Congratulations! You guessed the number in {count} tries!")
+    print(game_instruction)
+    while True:
+        level_choice = validate_number("Which level would you like to try? : ")
+            
+        if level_choice == 1:
+            target_number, min_limit, max_limit = generate_number(1)
+            attempt_left = 5
+            print(f"\nYou selected the Easy Level!\nTry to guess the number between {min_limit} and {max_limit}.\nYou have {attempt_left} tries. Good luck!\n")
             break
-        elif user_guess > target_number:
-            print("Too high! Try a lower number.")
+        elif level_choice == 2:
+            target_number, min_limit, max_limit = generate_number(2)
+            attempt_left = 6
+            print(f"\nYou picked Medium Level!\nGuess a number between {min_limit} and {max_limit}.\nYou have {attempt_left} tries. Good luck!\n")
+            break
+        elif level_choice == 3:
+            target_number, min_limit, max_limit = generate_number(3)
+            attempt_left = 7
+            print(f"\nYou picked Hard Level!\nGuess a number between {min_limit} and {max_limit}.\nYou have {attempt_left} tries. Good luck!\n")
+            break
+        elif level_choice == 4:
+            target_number, min_limit, max_limit = generate_number(4)
+            attempt_left = validate_number("How many guesses would you like?")
+            print(f"\nYou picked Custom Level!\nGuess a number between {min_limit} and {max_limit}.\nYou have {attempt_left} tries. Good luck!\n")
+            break
         else:
-            print("Too low! Try a higher number.")
-        attempt_left -= 1
-        print(f"You have only {attempt_left} attempt left\n")
+            print("\nOops, that's not a valid level. Please pick a number from 1 to 4.\n")
+            continue
+    
+    count = 0
+   
+    while attempt_left > 0:
+        user_guess = validate_number("Enter Your Guess : ")     
+        if user_guess < min_limit or user_guess > max_limit:
+            print(f"\nThat guess is  out of range,\nPlease enter an number between {min_limit} and {max_limit}\n")
+        else:
+            count += 1
+            if user_guess == target_number:
+                print(f"Congratulations! You guessed the number in {count} tries!")
+                break
+            elif user_guess > target_number:
+                print("Too high! Try a lower number.")
+            else:
+                print("Too low! Try a higher number.")
+            attempt_left -= 1
+            print(f"You have only {attempt_left} attempt left\n")
 
-        if attempt_left <= 0:
-            print("Your Attempts are OVER!!!")
-            print(
-                f"Sorry, you didn't guess the number. It was {target_number}.")
+            if attempt_left <= 0:
+                print("Game over! You have used all your attempts.")
+                print(f"The correct number was {target_number}.\nBetter luck next time!\n")
 
 
 if __name__ == "__main__":
